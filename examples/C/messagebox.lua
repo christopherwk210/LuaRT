c5460a4ea788c39e52f617932a59deb26d006375
samyeyo <samir.tine@luart.org>
2025-05-05 23:30:45 +0200

    Fixed example for x86

--- examples/C/messagebox.lua
+++ examples/C/messagebox.lua
@@ -8,7 +8,7 @@ local c = require "c"
 local user32 = c.Library("user32.dll")
 
 -- prototype of the Windows API function MessageBoxW()
-user32.MessageBoxW = "(lWWl)B"
+user32.MessageBoxW = "(pWWS)B"
 
 -- Execute MessageBox() with seamless Lua string to C wide string conversion
-user32.MessageBoxW(0, "Hello World !", "LuaRT C FFI module", 1)
+user32.MessageBoxW(c.NULL, "Hello World !", "LuaRT C FFI module", 1)
